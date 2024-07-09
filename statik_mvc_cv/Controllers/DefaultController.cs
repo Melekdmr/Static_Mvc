97197a0fcd9b07ddceecff2144507f2a554fe31f
melek <melek@MELEKDMR>
2024-07-09 14:32:51 +0300

    Proje dosyası ekle.

---
+++ statik_mvc_cv/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace statik_mvc_cv.Controllers
+{
+	public class DefaultController : Controller
+	{
+		//Get:Default
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ statik_mvc_cv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace statik_mvc_cv.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
